13e089c2d6b2747b36871e3b97bf7af333a546f3
MSalehJ <M.jalalian1999@gmail.com>
2021-07-31 17:00:38 +0430

    first commit

---
+++ trips/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView # new
+
+from .serializers import LogInSerializer, UserSerializer
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    
+
+class LogInView(TokenObtainPairView): # new
+    serializer_class = LogInSerializer
